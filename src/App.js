08fe227fdbfc0a97c6612bb9c488bbcefa52223a
Osman Goni <osmangoni@pop-os.localdomain>
2021-12-03 22:10:23 +0600

    some fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login/Login";
 import Order from "./components/Order/Order";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
